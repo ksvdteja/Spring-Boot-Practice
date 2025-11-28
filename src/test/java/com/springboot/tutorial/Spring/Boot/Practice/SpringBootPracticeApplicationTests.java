43fc75a585ec095bcf83936c934d51effe52320b
ksvdteja <divyateja773@gmail.com>
2025-11-29 00:10:31 +0530

    initial commit

---
+++ src/test/java/com/springboot/tutorial/Spring/Boot/Practice/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.tutorial.Spring.Boot.Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
